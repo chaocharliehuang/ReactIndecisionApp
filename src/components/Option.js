a6c5f56a168e517633e1a0a0a8d343e84941f67d
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-10-12 20:31:38 -0700

    refactored all stateless functional components to implicitly return JSX

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Option = (props) => {
-  return (
-    <div>
-      {props.text}
-      <button
-        onClick={(e) => {
-          props.handleDeleteOption(props.text);
-        }}>
-        Remove
-      </button>
-    </div>
-  );
-};
+const Option = (props) => (
+  <div>
+    {props.text}
+    <button
+      onClick={(e) => {
+        props.handleDeleteOption(props.text);
+      }}>
+      Remove
+    </button>
+  </div>
+);
 
 export default Option;
